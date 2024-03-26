22aaa8216008f08a3576ebd5f573c332f5809a19
Peter Kwok <peter.kwok94@gmail.com>
2024-03-26 14:19:38 -0400

    1:30:53, testing state, before, before each blocks

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Unit Testing
     BDD (Behavior Driven Development), Mocha, Chai
-        https://www.youtube.com/watch?v=u5cLK1UrFyQ - Up to: https://youtu.be/u5cLK1UrFyQ?t=5453 
+        https://www.youtube.com/watch?v=u5cLK1UrFyQ - Up to: https://youtu.be/u5cLK1UrFyQ?t=5911
--- tests/tests.js
+++ tests/tests.js
@@ -10,43 +10,9 @@ describe('Mocha', () => {
 })
 
 describe('checkForShip', () => {
-    it('should correctly report no ship at a given player\'s coorindates', () => {
-        const player = {
-            ships: [
-                {
-                    location: [[0, 0]]
-                }
-            ]
-        }
-        expect(checkForShip(player, [9, 9])).to.be.false
-    })
-
-    it('should correctly report a ship at a given player\'s coorindates', () => {
-        const player = {
-            ships: [
-                {
-                    location: [[0, 0]]
-                }
-            ]
-        }
-        expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
-    })
-
-    it('should handle ships at more than one coorindate', () => {
-        const player = {
-            ships: [
-                {
-                    location: [[0, 0], [0, 1]]
-                }
-            ]
-        }
-        expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0])
-        expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
-        expect(checkForShip(player, [9, 9])).to.be.false
-    })
-
-    it('should handle multiple ships', () => {
-        const player = {
+    let player // Setup phase, before
+    before(() => {
+        player = {
             ships: [
                 {
                     location: [[0, 0], [0, 1]],
@@ -59,6 +25,23 @@ describe('checkForShip', () => {
                 }
             ]
         }
+    })
+
+    it('should correctly report no ship at a given player\'s coorindates', () => {
+        expect(checkForShip(player, [9, 9])).to.be.false
+    })
+
+    it('should correctly report a ship at a given player\'s coorindates', () => {
+        expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
+    })
+
+    it('should handle ships at more than one coorindate', () => {
+        expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0])
+        expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
+        expect(checkForShip(player, [9, 9])).to.be.false
+    })
+
+    it('should handle multiple ships', () => {
         expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0])
         expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
         expect(checkForShip(player, [1, 0])).to.deep.equal(player.ships[1])
@@ -75,14 +58,16 @@ describe('damageShip', () => {
             damage: []
         }
         damageShip(ship, [0, 0])
+
         expect(ship.damage).to.not.be.empty
         expect(ship.damage[0]).to.deep.equal([0, 0]) // js arrays are distinct objects and not equal even if they have the same values. 
     })
 })
 
 describe('fire', () => {
-    it('should record damage on the given players ship at a given coorindate', () => {
-        const player = {
+    let player
+    beforeEach(() => { // because fire() mutates state, running it repeatedly will invalidate further tests. so, we reset state before each test. 
+        player = {
             ships: [
                 {
                     location: [[0, 0]],
@@ -90,7 +75,9 @@ describe('fire', () => {
                 }
             ]
         }
+    })
 
+    it('should record damage on the given players ship at a given coorindate', () => {
         fire(player, [0, 0])
 
         expect(player.ships[0].damage[0]).to.deep.equal([0, 0])
